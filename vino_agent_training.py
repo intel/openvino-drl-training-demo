a4dc6f78606178a3cf6b303da8436c802f239a1c
Siddharth Mehta <siddharth1.mehta@intel.com>
2021-04-02 17:03:38 -0700

    updated to stable baselines 3 + changed algo to SAC

--- vino_agent_training.py
+++ vino_agent_training.py
@@ -1,16 +1,17 @@
 import gym
 import panda_hover
-from stable_baselines.common.policies import MlpPolicy
-from stable_baselines import PPO1
+from stable_baselines3 import SAC
 import time
 
 env = gym.make('panda_hover-v0', gui=False, vino=False, device='CPU' )
+#env = make_vec_env(env, n_envs=1)
 
-model = PPO1(MlpPolicy, env, verbose=1, tensorboard_log="./ppo_log/")
+model = SAC("MlpPolicy", env, verbose=1, tensorboard_log="./sac_log/")
+
 start = time.time()
 model.learn(total_timesteps=10000)
 final = time.time()
-model.save("ppo_hover_agent")
+model.save("sac_hover_agent")
 
 print("Total Time (s): ", final-start)
 print("Model Load Time (s): ", env.model_load_time)
