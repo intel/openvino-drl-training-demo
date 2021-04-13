8cca41f4266cca2a1dc466621d53e4482c8e9c3c
Siddharth Mehta <siddharth1.mehta@intel.com>
2021-04-12 17:21:59 -0700

    changed to stablebaselines 3

--- inference.py
+++ inference.py
@@ -1,25 +1,24 @@
 import gym
 import panda_hover
-from stable_baselines.common.policies import MlpPolicy
-from stable_baselines import PPO1
-import time
+from stable_baselines3 import SAC
 
-env = gym.make('panda_hover-v0', gui=True)
-model = PPO1.load("ppo_hover_agent", verbose=0)
-model.set_env(env)
+env = gym.make('panda_hover-v0', gui=False)
+model = SAC.load("sac_hover_agent")
 
 print("Starting Inference")
+success = 0
+num_inferences = 25
 for x in range(0, 25):
     is_done = False
     obs = env.reset()
-    print(obs)
     steps = 0
     while not is_done:
         action, _ = model.predict(obs)
         obs, reward, is_done,_ = env.step(action)
         #env.render()
-
         steps = steps + 1
-    #print(env._q)
-    if steps > 15:
-        print("Goal Not Reached")
+
+    if reward == 2000:
+        success = success + 1
+
+print("Succes Rate %: ", (100*success)/num_inferences)
