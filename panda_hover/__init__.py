1757ddfabb5130298666922059134c514110f210
Mehta, Siddharth1 <siddharth1.mehta@intel.com>
2021-04-21 09:23:52 -0700

    Deleted panda_hover/envs/panda/meshes/collision/finger.stl, panda_hover/envs/panda/meshes/collision/hand.stl, panda_hover/envs/panda/meshes/collision/link0.stl, panda_hover/envs/panda/meshes/collision/link1.stl, panda_hover/envs/panda/meshes/collision/link2.stl, panda_hover/envs/panda/meshes/collision/link3.stl, panda_hover/envs/panda/meshes/collision/link4.stl, panda_hover/envs/panda/meshes/collision/link5.stl, panda_hover/envs/panda/meshes/collision/link6.stl, panda_hover/envs/panda/meshes/collision/link7.stl, panda_hover/envs/panda/meshes/visual/finger.stl, panda_hover/envs/panda/meshes/visual/hand.stl, panda_hover/envs/panda/meshes/visual/link0.stl, panda_hover/envs/panda/meshes/visual/link1.stl, panda_hover/envs/panda/meshes/visual/link2.stl, panda_hover/envs/panda/meshes/visual/link3.stl, panda_hover/envs/panda/meshes/visual/link4.stl, panda_hover/envs/panda/meshes/visual/link5.stl, panda_hover/envs/panda/meshes/visual/link6.stl, panda_hover/envs/panda/meshes/visual/link7.stl, panda_hover/envs/panda/meshes/visual_backup/finger.stl, panda_hover/envs/panda/meshes/visual_backup/hand.stl, panda_hover/envs/panda/meshes/visual_backup/link0.stl, panda_hover/envs/panda/meshes/visual_backup/link1.stl, panda_hover/envs/panda/meshes/visual_backup/link2.stl, panda_hover/envs/panda/meshes/visual_backup/link3.stl, panda_hover/envs/panda/meshes/visual_backup/link4.stl, panda_hover/envs/panda/meshes/visual_backup/link5.stl, panda_hover/envs/panda/meshes/visual_backup/link6.stl, panda_hover/envs/panda/meshes/visual_backup/link7.stl, panda_hover/envs/panda/ball.urdf, panda_hover/envs/panda/panda.urdf, panda_hover/envs/panda/panda_gripper.urdf, panda_hover/envs/__init__.py, panda_hover/envs/ft_inf.py, panda_hover/envs/panda_hover_env.py, panda_hover/envs/single_goal_classifier_resnet, panda_hover/envs/single_goal_classifier_resnet.onnx, panda_hover/envs/vino_ft_inf.py, panda_hover/__init__.py, classifier_training.py, data_gen.py, env_test.py, inference.py, requirements.txt, reward_classifier_check.py, vino_agent_training.py, 4_n.png files

--- panda_hover/__init__.py
+++
@@ -1,6 +0,0 @@
-from gym.envs.registration import register
-
-register(
-    id='panda_hover-v0',
-    entry_point='panda_hover.envs:PandaHoverEnv',
-)
--- panda_hover/envs/__init__.py
+++
@@ -1,1 +0,0 @@
-from panda_hover.envs.panda_hover_env import PandaHoverEnv
