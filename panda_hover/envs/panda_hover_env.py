ad82d11851b7cd54bbb491c79c32073a98a016ce
mehtas73 <siddharth1.mehta@intel.com>
2021-11-02 15:28:49 -0700

    added license

--- panda_hover/envs/panda_hover_env.py
+++ panda_hover/envs/panda_hover_env.py
@@ -1,4 +1,17 @@
-from .openvino_model import OpenVinoModel
+# Copyright (C) 2021 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions
+# and limitations under the License.from .openvino_model import OpenVinoModel
+
 from .pytorch_model import PyTorchModel
 from gym import spaces
 import numpy as np
